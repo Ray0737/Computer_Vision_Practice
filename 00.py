7ee380327022c85da97bdac48f25ef81eb42b1b1
c0der <raphee.rattanamanoonporn@gmail.com>
2026-01-08 21:43:34 +0700

    Update 00.py

--- 00.py
+++ 00.py
@@ -1,8 +1,8 @@
 import cv2 as cv
 import numpy as np
 
-# Image Show
-img = cv.imread("C:/Users/LENOVO/Pictures/Screenshots/UI.png")
+#--------------------------------------------------Image Show--------------------------------------------------#
+img = cv.imread("m5python/Aj phoom-my work/023546_school/R.jpg")
 cv.imshow('Test', img)
 cv.waitKey(0)
 
@@ -17,8 +17,8 @@ while True:
 capture.release()
 cv.destroyAllWindows()
 
-#Resize
-img = cv.imread("C:/Users/LENOVO/Pictures/Screenshots/UI.png")
+#--------------------------------------------------Resize--------------------------------------------------#
+img = cv.imread("m5python/Aj phoom-my work/023546_school/R.jpg")
 
 def rescaleFrame(frame,scale=0.75):
     width = int(frame.shape[1]*scale)
@@ -34,3 +34,61 @@ def changeRes(width,height):
 frame_resized = rescaleFrame(img)
 cv.imshow('Resized',frame_resized)
 cv.waitKey(0)
+
+# --------------------------------------------------Blank Page--------------------------------------------------#
+
+blank = np.zeros((500,500,3),dtype='uint8') #blank page
+cv.imshow('Blank',blank)
+
+img = cv.imread("m5python/Aj phoom-my work/023546_school/R.jpg")
+cv.imshow('Test', img)
+
+blank[200:300,300:400] = 0,0,255 # red
+cv.imshow('Red',blank)
+
+cv.rectangle(blank,(0,0),(250,500),(0,0,255),thickness=cv.FILLED) #250,500 = width x height
+cv.imshow('Rectangle',blank)
+cv.rectangle(blank,(0,0),(blank.shape[1]//2,blank.shape[0]//2),(0,0,255),thickness=cv.FILLED) #square
+cv.imshow('Rectangle',blank)
+cv.circle(blank,(blank.shape[1]//2,blank.shape[0]//2),40,(0,0,255),thickness=3)
+cv.imshow('Circle',blank)
+cv.line(blank,(0,0),(blank.shape[1]//2,blank.shape[0]//2),(0,0,255),thickness=3)
+cv.imshow('line',blank)
+cv.putText(blank,"Hello",(0,255),cv.FONT_HERSHEY_TRIPLEX,1.0,(0,255,0),2)
+cv.imshow('Text',blank)
+cv.waitKey(0)
+
+# --------------------------------------------------Crop--------------------------------------------------#
+
+img = cv.imread('m5python/Aj phoom-my work/023546_school/R.jpg')
+# Select rows 80 to 280 and columns 150 to 330
+cropped_image = img[80:280, 150:330] 
+
+cv.imshow("cropped", cropped_image)
+cv.imwrite("Cropped Image.jpg", cropped_image) # Saves the new image
+cv.waitKey(0)
+
+# --------------------------------------------------Rotation--------------------------------------------------#
+
+# Load the image
+img = cv.imread('m5python/Aj phoom-my work/023546_school/R.jpg')
+(h, w) = img.shape[:2] # Get image height and width
+
+# 1. Define the rotation parameters
+center = (w // 2, h // 2)  # Rotate around the center of the image
+angle = 45                 # Degrees to rotate (positive for counter-clockwise)
+scale = 1.0                # Keep the same scale (no zoom)
+
+# 2. Get the rotation matrix (OpenCV helper function)
+# This creates the 2x3 matrix mentioned in the document
+matrix = cv.getRotationMatrix2D(center, angle, scale)
+
+# 3. Apply the rotation using warpAffine
+# This moves every pixel to its new rotated position
+rotated_image = cv.warpAffine(img, matrix, (w, h))
+
+# Display the results
+cv.imshow("Original", img)
+cv.imshow("Rotated Image", rotated_image)
+cv.waitKey(0)
+cv.destroyAllWindows()
