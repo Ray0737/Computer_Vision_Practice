8fa253b0204b11889b8ca14ea4bf4fbd4fc692c3
c0der <raphee.rattanamanoonporn@gmail.com>
2026-01-10 11:27:45 +0700

    Update Face Detection.py

--- Face Detection.py
+++ Face Detection.py
@@ -33,4 +33,30 @@ else:
 
 cv.imshow("Face detect",frame_resized)
 cv.waitKey(0)
+
+cv.destroyAllWindows()
+
+#--------------------------------------------------Face Detection (live cam)--------------------------------------------------#
+
+cap = cv.VideoCapture(0)
+xml_path = cv.data.haarcascades + 'haarcascade_frontalface_default.xml'
+face_cascde = cv.CascadeClassifier(xml_path)
+
+while (cap.isOpened()):
+    chack, frame = cap.read()
+    
+    if chack:
+        gray_cap = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
+        face_detect = face_cascde.detectMultiScale(gray_cap)
+        for (x, y, w, h) in face_detect:
+            cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
+        cv.imshow("face detect", frame)
+        if cv.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if no frame is received (end of video or camera error)
+        break
+
+# Remember to release the capture and close windows when done
+cap.release()
 cv.destroyAllWindows()
